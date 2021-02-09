ca4c3e3055d1da1202f5379764f76f730fbb90e4
Malik-94 <yusuper1995@gmail.com>
2021-02-09 10:47:21 +0600

    Работа с календарем

---
+++ src/views/Сalendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="calendar">
+    <header>
+      <slot name="headerSlot" :header="header" />
+    </header>
+    <transition :name="transitionName" mode="out-in">
+    <div class="calendar__cells" :key="deltaChangedCounter">
+      <slot name="topDaysSlot" :days="topDays" />
+      <slot name="daysSlot" :days="days" />
+    </div>
+    </transition>
+  </section>
+</template>
